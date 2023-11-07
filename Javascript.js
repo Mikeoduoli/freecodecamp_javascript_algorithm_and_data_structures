a3208c803a38873f9a1523351805ebf9bfc8d564
Mikeoduoli <mikeoduoli@gmail.com>
2023-11-07 07:41:40 +0300

    DNA Pairing

--- Javascript.js
+++ Javascript.js
@@ -726,3 +726,25 @@ whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: 
     return str.replace(before, applyCasing(before, after));
   }
 
+  //DNA PAiring
+
+  function pairElement(str) {
+    const matchWithBasePair = function (char) {
+        switch(char){
+            case "A":
+                return ["A", "T"];
+            case "T":
+                return ["T", "A"];
+            case "C":
+                return ["C", "G"];
+            case "G":
+                return ["G", "C"];
+        }
+    };
+
+    const pairs = [];
+    for(let i=0; i <str.length; i++) {
+        pairs.push(matchWithBasePair(str[i]));
+    }
+    return pairs;
+  }
