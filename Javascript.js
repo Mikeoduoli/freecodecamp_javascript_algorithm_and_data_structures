9aea5e2a5e41547f3e6a43361c20de3e102b0a43
Mikeoduoli <mikeoduoli@gmail.com>
2023-11-13 21:58:31 +0300

    Iterate and Drop

--- Javascript.js
+++ Javascript.js
@@ -860,3 +860,13 @@ whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: 
   }
   
   console.log(smallestCommons([1,5]));
+
+  //Drop it
+  function dropElements(arr, func) {
+    while(arr.length > 0 && !func(arr[0])){
+      arr.shift();
+    }
+    return arr;
+  }
+  
+  console.log(dropElements([1, 2, 3], function(n) {return n < 3; }));
