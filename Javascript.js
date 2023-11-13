2edfbb70d8168552c8014fe825965552165d00e5
Mikeoduoli <mikeoduoli@gmail.com>
2023-11-13 07:40:11 +0300

    Sum of All Odd numbers

--- Javascript.js
+++ Javascript.js
@@ -819,3 +819,25 @@ whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: 
   
 
   console.log(sumFibs(4));
+
+  //Sum of All Prime
+  function sumPrimes(num) {
+    function isPrime(num) {
+      let sqrt = Math.sqrt(num);
+      for(let i = 2; i <= sqrt; i++) {
+        if (num % i ===0)
+          return false;
+      }
+      return true
+    }
+  
+    let sum  = 0
+    for (let i = 2; i <= num; i++) {
+      if (isPrime(i))
+      sum += i
+    }
+  
+    return sum;
+  }
+  
+  console.log(sumPrimes(10));
