eecf2077ebefe0a4f8748ab69399f79d45d9aa0e
Mikeoduoli <mikeoduoli@gmail.com>
2023-11-17 07:25:13 +0300

    5 JavaScript projects

--- Javascript.js
+++ Javascript.js
@@ -1047,3 +1047,51 @@ whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: 
   }
   
   console.log(telephoneCheck("555-555-5555"));
+
+  //Loops, Arrays Objects and methods for a Cash Register
+  function checkCashRegister(price, cash, cid) {
+    let change = cash * 100 - price * 100;
+    let cidTotal = 0;
+  
+    for (let elem of cid) {
+      cidTotal += elem[1] * 100;
+    }
+    if(change > cidTotal) {
+      return {status: "INSUFFICIENT_FUNDS", change: []};
+    } else if (change === cidTotal){
+      return {status: "CLOSED", change: cid};
+    } else {
+      let result = [];
+      cid = cid.reverse();
+  
+      let currencyUnit = {
+        "ONE HUNDRED" : 10000,
+        "TWENTY" : 2000,
+        "TEN" : 1000,
+        "FIVE" : 500,
+        "ONE" : 100,
+        "QUARTER" : 25,
+        "DIME" : 10,
+        "NICKEL" : 5,
+        "PENNY" : 1
+      }
+      for(let elem of cid){
+        let moneyHolder = [elem[0], 0];
+        elem[1] = elem[1] * 100;
+        while (change >= currencyUnit[elem[0]] && elem[1] > 0) {
+          change -=currencyUnit[elem[0]];
+          elem[1] -= currencyUnit[elem[0]];
+          moneyHolder[1] += currencyUnit[elem[0]] / 100;
+        }
+        if (moneyHolder[1] > 0) {
+          result.push(moneyHolder);
+        }
+      }
+      if (change > 0) {
+        return {status: "INSUFFICIENT_FUNDS", change: []};
+      }
+      return {status: "OPEN", change: result}
+    }
+  }
+  
+  console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
