79c376207dc0a05701109bab4b526928c095ba24
Mikeoduoli <mikeoduoli@gmail.com>
2023-12-21 08:19:02 +0300

    DOM Manipulation

---
+++ JohnHopkins_DOM/index.js
@@ -0,0 +1,22 @@
+// DOM Manipulation
+// console.log(document.getElementById("title"));
+
+function sayHello() {
+    var name = 
+        document.getElementById("name").value;
+        // var message = "Hello " + name + "!";
+        var message = "<h2>Hello " + name + "!</h2>";
+
+    document
+        .getElementById("content")
+        .innerHTML = message;
+
+    if(name === "student"){
+        var title =
+            document.querySelector("#title")
+            .textContent;
+        title += " & Loving It!"
+        document.querySelector("#title")
+            .textContent = title;
+    }
+} 
