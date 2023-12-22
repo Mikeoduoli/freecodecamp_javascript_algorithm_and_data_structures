2f9a313d3b935744b6ac05442056c78d7dd185e7
Mikeoduoli <mikeoduoli@gmail.com>
2023-12-22 16:38:33 +0300

    DOMContentLoaded

--- JohnHopkins_DOM/index.js
+++ JohnHopkins_DOM/index.js
@@ -1,26 +1,35 @@
 // DOM Manipulation
 // console.log(document.getElementById("title"));
 
-function sayHello() {
-    var name = 
-        document.getElementById("name").value;
-        // var message = "Hello " + name + "!";
-        var message = "<h2>Hello " + name + "!</h2>";
+document.addEventListener("DOMContentLoaded", 
+    function (event) {
 
-    document
-        .getElementById("content")
-        .innerHTML = message;
-
-    if(name === "student"){
-        var title =
-        document
-            .querySelector("#title")
-            .textContent;
-        title += " & Loving It!"
-        document.querySelector("#title")
-            .textContent = title;
+        function sayHello (event) {
+            this.textContent = "Said it!"
+            var name = 
+                document.getElementById("name").value;
+                // var message = "Hello " + name + "!";
+                var message = "<h2>Hello " + name + "!</h2>";
+        
+            document
+                .getElementById("content")
+                .innerHTML = message;
+        
+            if(name === "student"){
+                var title =
+                document
+                    .querySelector("#title")
+                    .textContent;
+                title += " & Loving It!"
+                document.querySelector("#title")
+                    .textContent = title;
+            }
+        } 
     }
-} 
+    );
+
 
 
 //Event Handling
+// document.querySelector("button")
+//     .addEventListener("click", sayHello);
