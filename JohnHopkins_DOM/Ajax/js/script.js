043841465e76408c723e32b18dfb8fcd6c0243c8
Mikeoduoli <mikeoduoli@gmail.com>
2023-12-24 08:55:29 +0300

    Ajax II

---
+++ JohnHopkins_DOM/Ajax/js/script.js
@@ -0,0 +1,21 @@
+//Event Handling
+document.addEventListener("DOMContentLoaded", 
+    function (event) {
+        //Unobstrussive event binding
+        document.querySelector("button")
+            .addEventListener("click", function(){
+                // var self = this;
+                // var name = "";
+
+                //Call Server To get the name
+                $ajaxUtils
+                    .sendGetRequest("/data/name.txt",
+                        function (request) {
+                            var name = request.responseText;
+                            
+                            document.querySelector("#content")
+                                    .innerHTML = "<h2>Hello " + name + "!"
+                        });
+            });
+    }
+    )
